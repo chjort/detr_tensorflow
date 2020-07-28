78b4ac619ee0e1d41cff122c330cc78e4911b2a3
chjort <christoffer.hjort1995@gmail.com>
2020-07-28 23:47:42 +0200

    Finished implementing losses

--- loss_driver.py
+++ loss_driver.py
@@ -9,7 +9,6 @@ from chambers.utils.tf import pairwise_l1
 
 COCO_PATH = "/home/crr/datasets/coco"
 BATCH_SIZE = 2
-EOS_COEF = 0.1 # Non-box class loss weight
 
 # %%
 samples = np.load("x_pt.npy")
@@ -78,53 +77,51 @@ C_split = tf.split(C, sizes, -1)
 
 indices = [linear_sum_assignment(c[i]) for i, c in enumerate(C_split)]
 indices = [(tf.convert_to_tensor(i, dtype=tf.int32), tf.convert_to_tensor(j, dtype=tf.int32)) for i, j in indices]
-indices
 
 # %% # Get box assignments
-key_labels = [tf.gather(target["labels"], k_idx) for target, (q_idx, k_idx) in zip(targets, indices)]
-key_labels = tf.concat(key_labels, axis=0)
-key_labels = tf.cast(key_labels, tf.int32)
-
-query_labels = tf.fill([batch_size, n_box], n_class - 1)
-
 row_idx = tf.concat([tf.fill(q_idx.shape, i) for i, (q_idx, k_idx) in enumerate(indices)], axis=0)
 col_idx = tf.concat([q_idx for (q_idx, k_idx) in indices], axis=0)
-update_idx = tf.stack([row_idx, col_idx], axis=1)
+prediction_idx = tf.stack([row_idx, col_idx], axis=1)
 
-query_labels = tf.tensor_scatter_nd_update(query_labels, update_idx, key_labels)
+# get labels of predicted indices
+target_labels = [tf.gather(target["labels"], tgt_idx) for target, (prd_idx, tgt_idx) in zip(targets, indices)]
+target_labels = tf.concat(target_labels, axis=0)
+target_labels = tf.cast(target_labels, tf.int32)
+pred_labels = tf.fill([batch_size, n_box], n_class - 1)
+pred_labels = tf.tensor_scatter_nd_update(pred_labels, prediction_idx, target_labels)
 
-query_labels
+# get boxes of predicted indices
+target_boxes = tf.concat([tf.gather(target["boxes"], tgt_idx) for target, (prd_idx, tgt_idx) in zip(targets, indices)], axis=0)
+pred_boxes = tf.gather_nd(outputs["pred_boxes"], prediction_idx)
 
-query_labels_pt = tf.constant([[91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91,
-                                91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91,
-                                91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91,
-                                91, 32, 91, 91, 91, 1, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91,
-                                91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91,
-                                91, 91, 91, 91, 91, 91, 91, 91, 91, 91],
-                               [91, 91, 91, 91, 91, 62, 91, 91, 91, 1, 91, 67, 1, 91, 91, 91, 91, 91,
-                                31, 91, 91, 91, 91, 91, 91, 91, 91, 1, 91, 91, 91, 91, 91, 91, 1, 91,
-                                91, 91, 62, 1, 91, 91, 85, 91, 91, 67, 91, 91, 91, 91, 91, 85, 62, 1,
-                                91, 1, 91, 91, 1, 91, 91, 1, 91, 91, 91, 62, 91, 91, 91, 91, 91, 91,
-                                91, 91, 91, 91, 67, 91, 1, 91, 91, 91, 91, 91, 91, 91, 1, 91, 91, 91,
-                                91, 91, 18, 91, 91, 62, 91, 91, 1, 91]], dtype=tf.int32)
-
-tf.assert_equal(query_labels, query_labels_pt)
-
-# %%
-# TODO: Implement labels loss (Cross-entropy)
+# %% Weighted Cross-entropy
 y_pred = outputs["pred_logits"]
-y_true = tf.one_hot(query_labels, depth=n_class)
+y_true = tf.one_hot(pred_labels, depth=n_class)
 
-class_weights = tf.concat([tf.ones(n_class - 1, dtype=tf.float32), tf.constant([EOS_COEF], dtype=tf.float32)], axis=0)
-class_weights
+real_class_weight = 1
+non_class_weight = 0.1
+class_weights = tf.concat([tf.repeat(tf.constant(real_class_weight, dtype=tf.float32), n_class - 1),
+                           tf.constant([non_class_weight], dtype=tf.float32)],
+                          axis=0)
 
 weights = class_weights * y_true
 weights = tf.reduce_sum(weights, axis=-1)
-weights
 
-loss_ce = tf.nn.softmax_cross_entropy_with_logits(y_true, y_pred)
-# loss_ce = tf.losses.categorical_crossentropy(y_true, y_pred, from_logits=True)
-# loss_ce = tf.losses.categorical_crossentropy(y_true, tf.nn.log_softmax(y_pred, axis=-1))
+loss_ce = tf.keras.losses.categorical_crossentropy(y_true, y_pred, from_logits=True)
+loss_ce = loss_ce * weights
+loss_ce = tf.reduce_sum(loss_ce) / tf.reduce_sum(weights)
 
-tf.reduce_mean(loss_ce)
-tf.reduce_mean(loss_ce * weights)
+# %% Box losses
+pred_boxes.shape
+target_boxes.shape
+
+# L1 loss
+n_boxes = tf.cast(tf.shape(target_boxes)[0], tf.float32)
+loss_l1_box = tf.reduce_sum(tf.abs(target_boxes - pred_boxes)) / n_boxes
+
+# GIoU loss
+target_boxes = box_cxcywh_to_xyxy(target_boxes)
+pred_boxes = box_cxcywh_to_xyxy(pred_boxes)
+giou = boxes_giou(pred_boxes, target_boxes)
+giou = tf.linalg.diag_part(giou)
+loss_giou_box = tf.reduce_mean(1 - giou)
