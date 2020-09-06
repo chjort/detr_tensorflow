e5a3194d4111425398e34041ec1a17b2c0e86a86
chjort <christoffer.hjort1995@gmail.com>
2020-09-06 23:44:51 +0200

    dataset timer

--- chambers/utils/data.py
+++ chambers/utils/data.py
@@ -2,9 +2,16 @@ import time
 
 
 def time_dataset(dataset, epochs=1, n=None):
-    st = time.time()
-    for i, sample in dataset.repeat(epochs).enumerate():
-        print("{}/{}".format(i+1, n), end="\r")
+    epochs_times = []
+    for i in range(epochs):
+        st = time.time()
+        for j, sample in dataset.enumerate():
+            print("{}/{}".format(j+1, n), end="\r")
+        end_time = time.time() - st
+        print()
+        print(" -", end_time)
 
-    end_time = time.time() - st
-    return end_time
+        epochs_times.append(end_time)
+
+    print(sum(epochs_times) / len(epochs_times))
+    return epochs_times
