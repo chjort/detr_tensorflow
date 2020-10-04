f2eb0cf68ae5d3826311cc834607e2cef5d76412
chjort <christoffer.hjort1995@gmail.com>
2020-10-04 18:54:36 +0200

    Refactor

--- translate.py
+++ translate.py
@@ -3,37 +3,7 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-from chambers.layers.embedding import PositionalEmbedding1D
-from chambers.layers.transformer import Encoder, Decoder
-
-
-def Seq2SeqTransformer(input_vocab_size, output_vocab_size, embed_dim, num_heads, dim_feedforward,
-                       num_encoder_layers, num_decoder_layers, dropout_rate=0.1, name="seq2seq_transformer"):
-    inputs = tf.keras.layers.Input(shape=(None,), name="inputs_tokens")
-    targets = tf.keras.layers.Input(shape=(None,), name="targets_tokens")
-
-    x_enc = tf.keras.layers.Embedding(input_vocab_size, embed_dim, mask_zero=True, name="inputs_embed")(inputs)
-    x_enc = PositionalEmbedding1D(embed_dim, name="inputs_positional_encoding")(x_enc)
-    x = Encoder(embed_dim=embed_dim,
-                num_heads=num_heads,
-                ff_dim=dim_feedforward,
-                num_layers=num_encoder_layers,
-                dropout_rate=dropout_rate)(x_enc)
-
-    x_dec = tf.keras.layers.Embedding(output_vocab_size, embed_dim, mask_zero=True, name="targets_embed")(targets)
-    x_dec = PositionalEmbedding1D(embed_dim, name="targets_positional_encoding")(x_dec)
-    x = Decoder(embed_dim=embed_dim,
-                num_heads=num_heads,
-                ff_dim=dim_feedforward,
-                num_layers=num_decoder_layers,
-                dropout_rate=dropout_rate,
-                norm=False,
-                causal=True)([x_dec, x])
-
-    x = tf.keras.layers.Dense(output_vocab_size)(x)
-
-    model = tf.keras.models.Model(inputs=[inputs, targets], outputs=x, name=name)
-    return model
+from chambers.models.transformer import Seq2SeqTransformer
 
 
 class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
@@ -210,7 +180,7 @@ model.summary()
 learning_rate = CustomSchedule(embed_dim)
 optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
 loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
-metrics = [tf.keras.metrics.SparseCategoricalAccuracy()]
+metrics = [tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")]
 model.compile(optimizer=optimizer,
               loss=loss,
               metrics=metrics
@@ -223,14 +193,6 @@ model.fit(train_td,
           steps_per_epoch=steps_per_epoch,
           validation_data=val_td
           )
-
-# %%
-# it = iter(val_td)
-# (x1, x2), y = next(it)
-# z = model([x1, x2])
-
-# print(model.compute_mask([x1, x2], None).shape)
-# print(z._keras_mask.shape)
 
 # %%
 translator = Seq2SeqTranslator(tokenizer_pt, tokenizer_en, model, maxlen=MAX_LEN)
